b6dff2390525a62653394e340c015e653536fad1
Mariana de Araújo <marianabdaraujo@gmail.com>
2017-01-30 16:26:32 +0000

    use thrift function names in callbacks

--- wrappers/python/wpwithin_python/wpwithin_python/wpwithincallbacks.py
+++ wrappers/python/wpwithin_python/wpwithin_python/wpwithincallbacks.py
@@ -4,8 +4,8 @@ class AbstractEventListener(object):
     https://github.com/WPTechInnovation/worldpay-within-sdk/tree/feature/python-wrapper/wrappers/python/wpwithin_python/examples
     """
 
-    def begin_service_delivery(self, service_id, service_delivery_token, units_to_supply):
+    def beginServiceDelivery(self, service_id, service_delivery_token, units_to_supply):
         raise NotImplementedError("You should implement this method in a derived class.")
 
-    def end_service_delivery(self, service_id, service_delivery_token, units_received):
+    def endServiceDelivery(self, service_id, service_delivery_token, units_received):
         raise NotImplementedError("You should implement this method in a derived class.")
