c33a4018ef84b81ab6e3d9fa7432b2a848ae80cf
Conor Hackett <conor.hackett@worldpay.com>
2017-01-31 13:32:44 +0000

    Added function to obfuscate a HCE Card and also get a string representation of the HCE Card instance. Useful in logging.

--- sdkcore/wpwithin/types/hcecard.go
+++ sdkcore/wpwithin/types/hcecard.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 // HCECard represents details of a payment card
 type HCECard struct {
 	FirstName  string
@@ -9,4 +14,31 @@ type HCECard struct {
 	CardNumber string
 	Type       string
 	Cvc        string
+}
+
+// ObfuscateCardNumber obfuscates the card number with the exception of last four digits
+// i.e. 1234567890987654 becomes ************7654
+func (card HCECard) ObfuscateCardNumber() string {
+
+	pan := card.CardNumber
+
+	if len(pan) <= 4 {
+
+		return pan
+	}
+
+	var result = make([]string, 0)
+
+	for i := 1; i < len(pan)-4; i++ {
+
+		result = append(result, "*")
+	}
+	result = append(result, pan[len(pan)-4:len(pan)])
+
+	return strings.Join(result, "")
+}
+
+func (card HCECard) String() string {
+
+	return fmt.Sprintf("fname=%s;lname=%s;expm=%d;expy%d;num=%s;cvc=%s;type=%s;", card.FirstName, card.LastName, card.ExpMonth, card.ExpYear, card.ObfuscateCardNumber(), card.Cvc, card.Type)
 }
