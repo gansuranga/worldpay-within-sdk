739b68d5f8c6c115243e3b00baef4eb017cd200b
Andy Brodie <andy@locima.com>
2017-02-03 12:18:31 +0000

    Corrected logic to identify RPC Agent filename

--- wrappers/dotnet/Worldpay.Within/Worldpay.Within.Tests/RpcAgentConfigurationTest.cs
+++ wrappers/dotnet/Worldpay.Within/Worldpay.Within.Tests/RpcAgentConfigurationTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Worldpay.Innovation.WPWithin.AgentManager;
 
 namespace Worldpay.Within.Tests
@@ -15,5 +17,54 @@ namespace Worldpay.Within.Tests
             Assert.AreEqual(filename, cfg.Path);
         }
 
+
+        /// <summary>
+        ///     The existing available RPC Agents are:
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <term>rpc-agent-linux-arm</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-linux-32</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-linux-64</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-mac-32</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-mac-64</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-mac-arm-32</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-win-32.exe</term>
+        ///         </item>
+        ///         <item>
+        ///             <term>rpc-agent-win-64.exe</term>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        [TestMethod]
+        public void TestRpcAgentFilenameGenerator()
+        {
+            Assert.AreEqual("rpc-agent-win-64.exe",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.Amd64, PlatformID.Win32Windows, true));
+            Assert.AreEqual("rpc-agent-linux-arm",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.Arm, PlatformID.Unix, true));
+            Assert.AreEqual("rpc-agent-linux-arm",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.Arm, PlatformID.Unix, false));
+            Assert.AreEqual("rpc-agent-linux-32",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.X86, PlatformID.Unix, false));
+            // This is possibly an impossibility, X86 with 64-bit, but at least it should behave logically.
+            Assert.AreEqual("rpc-agent-linux-64",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.X86, PlatformID.Unix, true));
+            Assert.AreEqual("rpc-agent-mac-32",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.X86, PlatformID.MacOSX, false));
+            Assert.AreEqual("rpc-agent-mac-64",
+                RpcAgentFilenameGenerator.GetForEnvironment(ProcessorArchitecture.Amd64, PlatformID.MacOSX, true));
+        }
     }
 }
