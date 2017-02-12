9045513444e48fcb47ba0fc85c9501e917a9725e
kevingordonwp <kevin.gordon@worldpay.com>
2017-02-12 16:57:01 -0500

    launcer 2 fixed

--- wrappers/python_2-7/rpc.py
+++ wrappers/python_2-7/rpc.py
@@ -13,8 +13,9 @@ def startRPC(self, port, eventListenerPort):
       callbackPortString = " -callbackport=" + str(eventListenerPort)
   else:
   	  callbackPortString = ""
-  logging .debug(str(os.getcwd()) + "/rpc-agent" + "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
-  process = launcherLocal.launch(cfg, os.getcwd() + "/rpc-agent", "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
+  rpcAgentName = launcher2.getRpcAgentName()
+  logging .debug(str(os.getcwd()) + "/worldpay-within-rpc-agent-bins/" + rpcAgentName + "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
+  process = launcherLocal.launch(cfg, os.getcwd() + "/worldpay-within-rpc-agent-bins/" + rpcAgentName, "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
   return process
 
 def stopRPC(self, process):
